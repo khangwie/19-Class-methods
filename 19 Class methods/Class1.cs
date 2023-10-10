ddb0d1cc712616d1af3b5056c732d5e350635b6a
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-10 21:33:53 +0200

    Add project files.

---
+++ 19 Class methods/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Class_methods
+{
+    internal class student
+    {
+        public double Weight;
+        public string Name;
+        public int Age;
+
+        public student(double aWeight, string aName, int aAge)
+        {
+            this.Weight = aWeight;
+            this.Name = aName;
+            this.Age = aAge;
+        }
+
+        public bool IsObese()
+        {
+            if (Weight > 60)
+            {
+                return true;
+            }
+            else return false;
+
+        }
+    }
+}
---
+++ 19 Class methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Class_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student student1 = new student(70, "John", 15);
+            student student2 = new student(40, "Tim", 10);
+
+            Console.WriteLine(student1.IsObese());
+            Console.WriteLine(student2.IsObese());
+
+            Console.ReadLine();
+        }
+    }
+}
